bfa708517e8dcf7d5918fa81cf78fdba38d70b82
Slava Vidanov <vidslava@yandex.ru>
2017-08-11 20:31:32 +0300

    kernel: sched: cputime ifndef for sched_qhmp

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -81,8 +81,10 @@ void irqtime_account_irq(struct task_struct *curr)
 
 	if (account)
 		sched_account_irqtime(cpu, curr, delta, wallclock);
-/*	else if (curr != this_cpu_ksoftirqd())
-		sched_account_irqstart(cpu, curr, wallclock);*/
+#ifndef CONFIG_SCHED_QHMP
+ else if (curr != this_cpu_ksoftirqd())
+		sched_account_irqstart(cpu, curr, wallclock);
+#endif
 
 	local_irq_restore(flags);
 }
