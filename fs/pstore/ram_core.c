0456e424214451a3e584d4ef2607fa8857ae4534
Razziell <razziell@ya.ru>
2017-09-01 09:22:13 +0300

    fs: pstore: markw changes (reverted from commit f4f75a79eb776d7c121998a2f812f584374ad46b)

--- fs/pstore/ram_core.c
+++ fs/pstore/ram_core.c
@@ -295,7 +295,6 @@ ssize_t persistent_ram_ecc_string(struct persistent_ram_zone *prz,
 	return ret;
 }
 
-#ifndef CONFIG_MACH_XIAOMI_MARKW
 static void *memcpy_pstore(void *dest, const void *src, size_t count)
 {
 	char *tmp = dest;
@@ -305,16 +304,12 @@ static void *memcpy_pstore(void *dest, const void *src, size_t count)
 		*tmp++ = *s++;
 	return dest;
 }
-#endif
+
 static void notrace persistent_ram_update(struct persistent_ram_zone *prz,
 	const void *s, unsigned int start, unsigned int count)
 {
 	struct persistent_ram_buffer *buffer = prz->buffer;
-#ifdef CONFIG_MACH_XIAOMI_MARKW
-	memcpy(buffer->data + start, s, count);
-#else
 	memcpy_pstore(buffer->data + start, s, count);
-#endif
 	persistent_ram_update_ecc(prz, start, count);
 }
 
